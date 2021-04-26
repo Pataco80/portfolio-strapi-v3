02c086557ace9fcb8866b5f1a87c53a93e96c0a1
Ricardo Do Vale <pataco80@gmail.com>
2021-04-26 15:27:47 +0200

    start project update
    add all pages and components to starter project
    
    Add src and static folder to repository from John
    manage folders
    
    
    Add favicons and strapi configuration
    
    
    add package and scripts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,70 @@
 module.exports = {
   siteMetadata: {
-    title: "portfolio-strapi-v3",
+    title: 'WebDev Portfolio',
   },
   plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        icon: "src/images/icon.png",
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        // contentTypes: [`jobs`, `projects`, `blogs`, ],
+        //singleTypes:[`about` ]
+        contentTypes: [`jobs`, `projects`, `blogs`],
+        singleTypes: [`about`],
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: `gatsby-strapi-portfolio-project`,
+        short_name: `Portfolio`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: './src/images/favicons/android-chrome-512x512.png',
+        icons: [
+          {
+            src: './src/images/favicons/apple-touch-icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './src/images/favicons/favicon-16x16.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './src/images/favicons/favicon-32x32.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './src/images/favicons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './src/images/favicons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
       },
-      __key: "images",
     },
   ],
-};
+}
