de4a4d3bdfe7eb6504b24aad0128ef8cf23cabf1
Ricardo Do Vale <pataco80@gmail.com>
2021-04-27 23:50:06 +0200

    Create sidebar complete
    delete console.log

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,40 @@
-import React from "react"
-import links from "../constants/links"
-import socialLinks from "../constants/social_links"
-import { Link } from "gatsby"
-import { FaTimes } from "react-icons/fa"
-const Sidebar = () => {
-  return <h2>sidebar component</h2>
+import React from 'react'
+import links from '../constants/links'
+import socialLinks from '../constants/social_links'
+import { Link } from 'gatsby'
+import { FaTimes } from 'react-icons/fa'
+const Sidebar = ({ isOpen, toggleSidebar }) => {
+  return (
+    <aside className={isOpen ? 'sidebar show-sidebar' : 'sidebar'}>
+      <button className='close-btn' onClick={toggleSidebar}>
+        <FaTimes />
+      </button>
+      <div className='sidebar-container'>
+        <ul className={isOpen ? 'sidebar-links' : null}>
+          {links.map((link) => {
+            return (
+              <li>
+                <Link Key={link.id} to={link.url} onClick={toggleSidebar}>
+                  {link.text}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+        <ul className={isOpen ? 'social-links sidebar-icons' : null}>
+          {socialLinks.map((link) => {
+            return (
+              <li Key={link.id}>
+                <a href={link.url} className='social-link'>
+                  {link.icon}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </aside>
+  )
 }
 
 export default Sidebar
