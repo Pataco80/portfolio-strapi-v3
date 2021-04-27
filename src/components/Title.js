6491e19fecec9e479121109f0eb9526a958d79a3
Ricardo Do Vale <pataco80@gmail.com>
2021-04-27 17:09:36 +0200

    component title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,7 @@
-import React from "react"
+import React from 'react'
 
-const Title = () => {
-  return <h2>title component</h2>
+const Title = ({ title }) => {
+  return <h2>{title || 'default'}</h2>
 }
 
 export default Title
